82b7f7fa60c97b257fc4030074cc44235f58399f
SeMyeong Park <myeong@gupsa.com>
2025-05-14 14:50:15 +0900

    player 이름 입력창

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Player from "./assets/components/Player";
+
 function App() {
   return (
-    <p></p>
+    <div id="game-container">
+      <ol id="players">
+        <Player initialName="player1" symbol="X" />
+        <Player initialName="player2" symbol="O" />
+      </ol>
+      GMAE BOARD
+    </div>
   );
 }
 
---
+++ src/assets/components/Player.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    console.log(isEditing);
+  }, [isEditing]);
+
+  function handleEditing() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    let { target } = event;
+    setPlayerName(target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">
+          {isEditing ? <input type="text" value={playerName} onChange={handleChange} required /> : playerName}
+        </span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditing}>{isEditing ? "save" : "edit"}</button>
+    </li>
+  );
+}
+
+export default Player;
